4923311141cda48049ea3c3670e6009abec3e2c1
Linkle123 <102257641+Linkle123@users.noreply.github.com>
2023-11-14 23:21:40 +0900

    Altered layouts and progress on basic features
    Worked on translate functionality, now working on saving to ankidroid functionality.
    Also changed flashcard layout to be simpler

--- app/src/main/java/com/example/koreantoenglishflashcardsaver/FlashCardAdapter.kt
+++ app/src/main/java/com/example/koreantoenglishflashcardsaver/FlashCardAdapter.kt
@@ -1,35 +1,50 @@
 package com.example.koreantoenglishflashcardsaver
 
 import android.content.Context
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.koreantoenglishflashcardsaver.Flashcard
 
-class FlashCardAdapter(private val context: Context)
+class FlashCardAdapter(private var cards : ArrayList<Flashcard>, private val onItemClick: (position: Int) -> Unit)
     : ListAdapter<Flashcard, FlashCardAdapter.ItemViewHolder>(FlashCardComparator()){
-        class ItemViewHolder(private val view: View)
+
+        class ItemViewHolder(private val view: View, private val onItemClick: (position: Int) -> Unit)
             : RecyclerView.ViewHolder(view){
-            val journal: ConstraintLayout = view.findViewById(R.id.flashcard)
-            var id : Int = -1
-            val word: EditText = view.findViewById(R.id.word_item)
-            val translation: EditText = view.findViewById(R.id.word_item_translation)
+            val word: TextView = view.findViewById(R.id.word_item)
+            val translation: TextView = view.findViewById(R.id.word_item_translation)
+            val closebutton: Button = view.findViewById(R.id.close_button)
+            var id: Int = -1
+            init {
+                closebutton.setOnClickListener {
+                    deleteCard()
+                }
+            }
+
+            private fun deleteCard() {
+                val position = absoluteAdapterPosition
+                onItemClick(position)
+            }
         }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val adapterLayout = LayoutInflater.from(parent.context).
             inflate(R.layout.flashcard_layout, parent, false)
-        return ItemViewHolder(adapterLayout)
+        return ItemViewHolder(adapterLayout){ position -> onItemClick(position) }
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        val item = getItem(position)
+        val item = cards[position]
         holder.word.setText(item.word)
         holder.translation.setText(item.translation)
+        holder.id = position
     }
 
     class FlashCardComparator : DiffUtil.ItemCallback<Flashcard>() {
--- app/src/main/java/com/example/koreantoenglishflashcardsaver/MainActivity.kt
+++ app/src/main/java/com/example/koreantoenglishflashcardsaver/MainActivity.kt
@@ -4,39 +4,71 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import androidx.activity.ComponentActivity
+import androidx.compose.ui.res.stringResource
 import androidx.recyclerview.widget.RecyclerView
 import com.google.cloud.translate.Translate
 import com.google.cloud.translate.TranslateOptions
 import com.google.cloud.translate.Translation
-
+import com.ichi2.anki.api.AddContentApi
 
 class MainActivity : ComponentActivity() {
     lateinit var adapter: FlashCardAdapter
+    lateinit var flashcards: ArrayList<Flashcard>
     var connected: Boolean = false
     lateinit var translate: Translate
-    val targetLanguage: String = "Korean"
+    val sourceLanguage: String = "ko"
+    val targetLanguage: String = "en"
     lateinit var inputText: EditText
+    lateinit var outputText: EditText
+
+    lateinit var ankiAPI: AddContentApi
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        adapter = FlashCardAdapter(this)
+        adapter = FlashCardAdapter(flashcards) { position -> onItemCloseClick(position) }
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter = adapter
         translate = TranslateOptions.getDefaultInstance().service
+
+        // Initialize the textfields to null
         inputText = findViewById(R.id.translate_text)
+        outputText = findViewById(R.id.translated_text)
+        inputText.text = null
+        outputText.text = null
+
+        // Initialize buttons
+        val translatebutton = findViewById<Button>(R.id.translate_button)
+        val addcardbutton = findViewById<Button>(R.id.add_card_button)
+        val savecardsbutton = findViewById<Button>(R.id.save_cards_button)
+
+        translatebutton.setOnClickListener{
+            translate()
+        }
+        addcardbutton.setOnClickListener {
+            addCard()
+        }
+
+        ankiAPI = AddContentApi(this)
+    }
+
+    private fun onItemCloseClick(position: Int) {
+        flashcards.removeAt(position)
+        adapter.notifyItemRemoved(position)
     }
 
 
-    fun translate() {
-        val translation: Translation = translate.translate(
+    fun translate(){
+        val response: Translation = translate.translate(
             inputText.text.toString(),
-            Translate.TranslateOption.sourceLanguage("ko"),
-            Translate.TranslateOption.targetLanguage("en"),
+            Translate.TranslateOption.sourceLanguage(sourceLanguage),
+            Translate.TranslateOption.targetLanguage(targetLanguage),
             Translate.TranslateOption.model("nmt"),
             Translate.TranslateOption.format("text")
         )
+        outputText.setText(response.translatedText)
     }
 
     fun checkInternetConnection(): Boolean {
@@ -48,5 +80,39 @@ class MainActivity : ComponentActivity() {
             connectivityManager!!.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)!!.state == NetworkInfo.State.CONNECTED ||
                     connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)!!.state == NetworkInfo.State.CONNECTED
         return connected
+    }
+
+    fun addCard() {
+        if(inputText.text != null && outputText.text != null) {
+            val card = Flashcard(inputText.text.toString(), outputText.text.toString())
+            inputText.text = null
+            outputText.text = null
+            flashcards.add(card)
+            adapter.notifyItemInserted(flashcards.size - 1);
+        }
+    }
+
+    fun saveAllCards() {
+
+    }
+
+    private fun getOrGenerateDeckId(): Long {
+        var did: Long? = getDeckId(this.resources.getString(R.string.deck_name))
+        if (did == null) {
+            did = ankiAPI.addNewDeck(this.resources.getString(R.string.deck_name))
+        }
+        return did!!
+    }
+
+    private fun getDeckId(deckName: String): Long? {
+        val deckList: Map<Long, String> = ankiAPI.getDeckList()
+        if (deckList != null) {
+            for ((key, value) in deckList) {
+                if (value.equals(deckName, ignoreCase = true)) {
+                    return key
+                }
+            }
+        }
+        return null
     }
 }
